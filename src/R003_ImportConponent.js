32db0508438d5df63d791f215cfffc4aa6f114b4
yoonji Kim <ky199247@gmail.com>
2022-08-30 18:02:26 +0900

    second commit

---
+++ src/R003_ImportConponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class R003_ImportConponent extends Component {
+    render () {
+        return (
+            <h2> [ THIS IS IMPORTED COMPONENT ] <br></br>추가는 아래로 </h2>
+        )
+    }  
+}
+
+export default R003_ImportConponent;
